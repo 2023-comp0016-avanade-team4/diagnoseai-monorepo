064bb6e37646ea076e0e4ff0443a96107874d5ae
James <lim.en.hui.james@gmail.com>
2024-01-08 19:07:27 +0000

    Add code to compress image and upload blob

--- core/chat_img/api.py
+++ core/chat_img/api.py
@@ -7,15 +7,105 @@ here, or in chat_history. Once confirmed, please remove this TODO.
 """
 
 import logging
-import puremagic
 import os
+from io import BytesIO
+from uuid import uuid4
 
 import azure.functions as func  # type: ignore[import-untyped]
+import puremagic  # type: ignore[import-untyped]
+from azure.storage.blob import BlobServiceClient
+from PIL import Image
+
+IMAGE_BLOB_CONNECTION_STRING = os.environ["ImageBlobConnectionString"]
+IMAGE_BLOB_CONTAINER = os.environ["ImageBlobContainer"]
+
+blob_service_client = BlobServiceClient.from_connection_string(
+    IMAGE_BLOB_CONNECTION_STRING)
 
 app = func.FunctionApp()
 
+
+def compress_image(content: bytes) -> bytes:
+    """
+    Calls a library to compress the content.
+
+    Args:
+        content (bytes): Content to compress
+
+    Returns:
+        bytes: The compressed image
+    """
+    # doing it this way because we want PIL to infer the image, and also
+    # automatically compress the image when saving
+    img_stream = BytesIO(content)
+    out_stream = BytesIO()
+    img = Image.open(img_stream)
+    img.save(out_stream, format='JPEG')
+    return out_stream.getvalue()
+
+
+def save_to_blob(filename: str, content: bytes):
+    """
+    Saves a blob to the Azure Blob Storage.
+
+    Args:
+        filename (str): Filename to save the blob as
+        content (bytes): Content in bytes to save
+    """
+    logging.info('Saving content to image blob storage as: %s', filename)
+    # saves the content in bytes into the azure blob specified by
+    # image endpoint
+    blob_client = blob_service_client.get_blob_client(
+        IMAGE_BLOB_CONTAINER, filename)
+    try:
+        blob_client.upload_blob(content)
+    # NOTE: It's okay to do a catch all, because all errors that can
+    # occur must come from uploading the blob / compressing the image
+    except Exception as e:  # pylint: disable=[broad-exception-caught]
+        logging.error('Unable to upload blob: %s', e)
+        raise e
+
+
+def save_reference_to_db(_conversation_id: str, _image_id: str):
+    """
+    Saves the image reference to the database as a history item.
+
+    Args:
+        conversation_id (str): Conversation ID to save as
+        image_id (str): Image ID. This should exist in the blob
+                        storage as {image_id}.jpg
+    """
+    # TODO: save the image. This is left empty for now because a
+    # database change is required, and that's another card
+
+
+def shadow_to_db(conversation_id: str, content: bytes):
+    """
+    Shadows the image upload to the database for history reasons.
+
+    This function is designed not to throw any errors, because the
+    failure to save an image should not prevent the model from
+    interpreting the image.
+
+    Args:
+        conversation_id (str): The conversation ID of this request
+        content (bytes): The bytes to save. This MUST be JPEG, and NO
+                         checks will be performed
+    """
+    iden = str(uuid4()).replace('-', '')
+    try:
+        save_to_blob(f"{iden}.jpg", content)
+        save_reference_to_db(conversation_id, iden)
+    except Exception:  # pylint: disable=[broad-exception-caught]
+        pass
+
+
 def handle_post(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    Handles the POST request.
+    """
     if 'conversation_id' not in req.params:
+        logging.info('Request does not contain conversation ID')
         return func.HttpResponse(
             status_code=400,
         )
@@ -24,13 +114,24 @@ def handle_post(req: func.HttpRequest) -> func.HttpResponse:
     body = req.get_body()
 
     if not body and not puremagic.from_string(body).startswith('image'):
+        logging.info('Request\'s body is unrecognized')
         return func.HttpResponse(
-            body="unsupported file type"
+            body="unsupported file type",
             status_code=400,
         )
 
-    # TODO: do something with the image
+    try:
+        compressed = compress_image(body)
+    except OSError:
+        logging.error('Cannot compress uploaded image')
+        return func.HttpResponse(
+            status_code=500,
+        )
 
+    # TODO: call Aadhik's function to do magical image inference
+    shadow_to_db(conversation_id, compressed)
+
+    logging.error('Server error')
     return func.HttpResponse(
         status_code=500,
     )
@@ -40,6 +141,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Chat Image called with %s', req.method)
     if req.method == 'POST':
         return handle_post(req)
+
+    logging.info('Fell through, no supported methods')
     return func.HttpResponse(
         status_code=200,
     )
