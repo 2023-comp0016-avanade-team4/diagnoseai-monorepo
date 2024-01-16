bd4abfbff37779b64bc4917cb2492dc59c526224
James <lim.en.hui.james@gmail.com>
2024-01-16 02:09:18 +0000

    Use snippet in actual loading code

--- src/app/validate/page.tsx
+++ src/app/validate/page.tsx
@@ -1,22 +1,24 @@
-'use client'
-import { Button } from "@nextui-org/react"
-import { useEffect, useRef, useState } from 'react'
-import ImageCarousel from "../components/ImageCarousel"
+"use client";
+import { Button } from "@nextui-org/react";
+import { useEffect, useRef, useState } from "react";
+import ImageCarousel from "../components/ImageCarousel";
 import Chat from "../components/Chat";
 
 const Validate = () => {
-  const fillerDivRef = useRef<HTMLDivElement>(null)
-  const [divHeight, setDivHeight] = useState(0)
-  const [divWidth, setDivWidth] = useState(0)
+  const fillerDivRef = useRef<HTMLDivElement>(null);
+  const [divHeight, setDivHeight] = useState(0);
+  const [divWidth, setDivWidth] = useState(0);
   // TODO: whoever is doing the backend, use setImages
   const [images, setImages] = useState<string[]>([
     "https://atlas-content-cdn.pixelsquid.com/stock-images/gas-boiler-Od6zKJ0-600.jpg",
     "https://atlas-content-cdn.pixelsquid.com/stock-images/thermex-boiler-n1Pm244-600.jpg",
-    "https://banner2.cleanpng.com/20180526/hst/kisspng-electric-boiler-industry-manufacturing-5b09a14f01b6b3.840125911527357775007.jpg"])
+    "https://banner2.cleanpng.com/20180526/hst/kisspng-electric-boiler-industry-manufacturing-5b09a14f01b6b3.840125911527357775007.jpg",
+  ]);
 
   // TODO: whoever is doing the backend, use setExtractedText
-  const [extractedText, setExtractedText] = useState<string>('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu suscipit diam. Ut vitae varius mauris, a pharetra erat. Suspendisse tempus eget orci vitae euismod. Fusce lacinia velit quam, quis sollicitudin urna sagittis et. Integer in lacus ac turpis fermentum interdum id mattis lacus. Praesent at condimentum tellus. Nunc dignissim neque id dui blandit consectetur. In at ultricies mi. Aliquam id risus ac elit consectetur porttitor eu at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque aliquam dui et laoreet porttitor. Proin tempor enim eu tortor laoreet, vitae pretium augue interdum.')
-
+  const [extractedText, setExtractedText] = useState<string>(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu suscipit diam. Ut vitae varius mauris, a pharetra erat. Suspendisse tempus eget orci vitae euismod. Fusce lacinia velit quam, quis sollicitudin urna sagittis et. Integer in lacus ac turpis fermentum interdum id mattis lacus. Praesent at condimentum tellus. Nunc dignissim neque id dui blandit consectetur. In at ultricies mi. Aliquam id risus ac elit consectetur porttitor eu at lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque aliquam dui et laoreet porttitor. Proin tempor enim eu tortor laoreet, vitae pretium augue interdum."
+  );
 
   // HACK (?): Not sure if this counts as a hack, since this was the only way I had to do this.,
   // Handles the resizing of the image carousel
@@ -25,36 +27,54 @@ const Validate = () => {
     // validation data is present before proceeding
 
     const handleResize = () => {
-      setDivHeight(0)
-      setDivWidth(0)
+      setDivHeight(0);
+      setDivWidth(0);
       setTimeout(() => {
-        setDivHeight(fillerDivRef.current?.clientHeight || 0)
-        setDivWidth(fillerDivRef.current?.clientWidth || 0)
-      })
-    }
+        setDivHeight(fillerDivRef.current?.clientHeight || 0);
+        setDivWidth(fillerDivRef.current?.clientWidth || 0);
+      });
+    };
 
-    window.addEventListener('resize', handleResize)
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
-    setDivHeight(fillerDivRef.current?.clientHeight || 0)
-    setDivWidth(fillerDivRef.current?.clientWidth || 0)
-  }, [fillerDivRef])
+    setDivHeight(fillerDivRef.current?.clientHeight || 0);
+    setDivWidth(fillerDivRef.current?.clientWidth || 0);
+  }, [fillerDivRef]);
+
+  useEffect(() => {
+    // TODO: replace with index from store
+    const searchIndex = "validation-index";
+    let response = fetch(`/api/indexContent?searchIndex=${searchIndex}`, {
+      method: "GET",
+    }).then((response) => {
+      response.json().then((data) => {
+        setExtractedText(data.results.join("\n"));
+      });
+    });
+  });
 
   return (
     <div className="flex h-full max-h-full">
       <div className="flex-1 max-h-full">
         <div className="p-4 bg-gray-200 h-full flex">
           <div className="grid grid-cols-1 grid-rows-[auto,1fr,1fr,auto] w-full max-h-full">
-            <h2 className="py-2 text-xl text-center font-extrabold">Validate</h2>
+            <h2 className="py-2 text-xl text-center font-extrabold">
+              Validate
+            </h2>
             <div className="bg-slate-100 flex-1 flex flex-col">
               <h3 className="p-3 text-md font-bold flex-0">Extracted Images</h3>
               <div ref={fillerDivRef} className="flex-1">
-                <ImageCarousel images={images} height={divHeight} width={divWidth} />
+                <ImageCarousel
+                  images={images}
+                  height={divHeight}
+                  width={divWidth}
+                />
               </div>
             </div>
             <div className="bg-slate-100 flex-1 flex flex-col overflow-y-auto">
@@ -63,7 +83,10 @@ const Validate = () => {
                 <p>{extractedText}</p>
               </div>
             </div>
-            <Button className="my-3 max-w-xs flex-0 place-self-center" color="primary">
+            <Button
+              className="my-3 max-w-xs flex-0 place-self-center"
+              color="primary"
+            >
               Confirm Validation
             </Button>
           </div>
@@ -79,7 +102,7 @@ const Validate = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
-export default Validate
+export default Validate;
