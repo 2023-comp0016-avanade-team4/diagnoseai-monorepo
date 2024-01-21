ab25b6b15b4cc8be278970cb0321bdabc838ec23
Aadhik Easwar <easwaraadhik@gmail.com>
2024-01-21 21:36:06 +0000

    Removing comments

--- core/test/test_image_vectorisation.py
+++ core/test/test_image_vectorisation.py
@@ -7,9 +7,6 @@ aoie_patch = patch('langchain.embeddings.AzureOpenAIEmbeddings').start()
 as_patch = patch('langchain.vectorstores.azuresearch.AzureSearch').start()
 cts_patch = patch('langchain.text_splitter.CharacterTextSplitter').start()
 environ_patch = patch.dict(os.environ, {"CognitiveSearchKey": "mock_key", "CognitiveSearchEndpoint": "mock_endpoint", "OpenAIKey": "mock_key", "OpenAIEndpoint": "mock_endpoint"}).start()
-#post_patch = patch('requests.post').start()
-
-#loads_patch = patch('json.loads').start()
 
 from core.utils.vectorise_text import vectoriseImageSummary
 
@@ -20,9 +17,6 @@ class TestImageSummaryVectorisation(unittest.TestCase):
     def test_vectoriseImageSummary(self):
         imageSummary = ""
         vectorIndex = "" 
-        #mock_response = { 'text' : "{'choices': [{'message': {'content': 'mock_content'}}]}"}
-        #requests.post.return_value = Mock(**mock_response)
-        #vectoriseImage(image_url, vectorIndex)
         vectoriseImageSummary(imageSummary, vectorIndex)
         aoie_patch.assert_called()
         as_patch.assert_called()
