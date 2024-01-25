82acee1bcfc2da27a3b563e2b245eb8a031c904f
aadhikeaswar <117028089+aadhikeaswar@users.noreply.github.com>
2024-01-25 21:12:49 +0000

    Update core/fileUploadTrigger/__init__.py
    Co-authored-by: James <jameshi16@users.noreply.github.com>

--- core/fileUploadTrigger/__init__.py
+++ core/fileUploadTrigger/__init__.py
@@ -40,6 +40,9 @@ embeddings = AzureOpenAIEmbeddings(
 
 
 def main(myblob: InputStream):
+"""
+Entrypoint to process blob storage event
+"""
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
