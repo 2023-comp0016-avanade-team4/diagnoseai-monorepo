d1139cce77ff6086ca9e2a879092aa5bfa4418db
Aadhik Easwar <easwaraadhik@gmail.com>
2024-01-28 17:34:03 +0000

    more linting work

--- core/test/test_blob_trigger.py
+++ core/test/test_blob_trigger.py
@@ -15,14 +15,22 @@ is_patch = patch('azure.functions.InputStream').start()
 
 #Environment patching
 
-environ_patch = patch.dict(os.environ, {"CognitiveSearchKey": "mock_key", "CognitiveSearchEndpoint": "mock_endpoint", "OpenAIKey": "mock_key", "OpenAIEndpoint": "mock_endpoint", "DocumentEndpoint" : "mock_endpoint", "DocumentKey" : "mock_key"}).start()
+environ_patch = patch.dict(os.environ, {
+    "CognitiveSearchKey": "mock_key",
+    "CognitiveSearchEndpoint": "mock_endpoint",
+    "OpenAIKey": "mock_key",
+    "OpenAIEndpoint": "mock_endpoint",
+    "DocumentEndpoint" : "mock_endpoint",
+    "DocumentKey" : "mock_key"
+    }).start()
 
 from core.fileUploadTrigger import main
 
 
 class TestFileUploadTrigger(unittest.TestCase):
     """ Class to test file upload trigger """
-    def testMain(self):
+    def test_main(self):
+        """ Trivial test for file upload trigger """
         main(is_patch)
         dac_patch.assert_called()
         dip_patch.assert_called()
@@ -30,9 +38,11 @@ class TestFileUploadTrigger(unittest.TestCase):
         aoie_patch.assert_called()
         as_patch.assert_called()
 
-    def testSearchIndexSplicing(self):
-        is_patch.name = "document-storage/2788c990-9822-11ee-8918-5dab27c25f8cAadhik_Easwar_Resume.pdf"
+    def test_search_index_splicing(self):
+        """ Tests the splicing of the search index name from the blob name"""
+        is_patch.name = "document-storage/2788c990-9822\
+                -11ee-8918-5dab27c25f8cAadhik_Easwar_Resume.pdf"
         main(is_patch)
         as_patch.assert_called()
-        self.assertEqual(as_patch.call_args.kwargs['index_name'], "2788c990-9822-11ee-8918-5dab27c25f8c")
-
+        self.assertEqual(as_patch.call_args.kwargs['index_name'],
+                "2788c990-9822-11ee-8918-5dab27c25f8c")
