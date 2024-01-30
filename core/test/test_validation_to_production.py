8b0c2b8d4c4287b214e555b8e69a1ad4d47b0e49
Aadhik Easwar <easwaraadhik@gmail.com>
2024-01-30 12:00:50 +0000

    Finished testing, added token verification, and fixed linting

--- core/test/test_validation_to_production.py
+++ core/test/test_validation_to_production.py
@@ -1,1 +1,101 @@
 """ Module for testing the validation_to_production endpoint. """
+
+import os
+import unittest
+from unittest.mock import patch
+
+from azure.functions import HttpRequest
+from azure.core.exceptions import HttpResponseError
+
+# Patching the verify_token function
+
+vjwt_patch = patch('utils.verify_token.verify_token').start()
+vjwt_patch.return_value = True
+
+# Globals patching
+akc_patch = patch('azure.core.credentials.AzureKeyCredential').start()
+sc_patch = patch('azure.search.documents.SearchClient').start()
+sic_patch = patch('azure.search.documents.indexes.SearchIndexClient').start()
+http_response_patch = patch('azure.functions.HttpResponse').start()
+verify_token_patch = patch('utils.verify_token.verify_token').start()
+
+os.environ['CognitiveSearchKey'] = 'mock-key'
+os.environ['CognitiveSearchEndpoint'] = 'mock-endpoint'
+os.environ['ProductionIndexName'] = 'mock-index'
+
+# pylint: disable=wrong-import-position
+from core.validation_to_production import main  # noqa: E402
+
+
+class TestValidationToProduction(unittest.TestCase):
+    """
+    Tests the validation_to_production endpoint
+    """
+    req = HttpRequest(
+        url="",
+        method='POST',
+        body=b'{"validationIndexName": "test"}',
+        headers={
+            'Auth-Token': 'test'
+        }
+    )
+
+    @patch('core.validation_to_production.prodClient')
+    @patch('core.validation_to_production.cogSearchClient')
+    def test_main(self, cog_search_client_patch, prod_client_patch):
+        """
+        Tests that the endpoint returns a 200 when authorized and validation
+        index exists
+        """
+        verify_token_patch.return_value = True
+        cog_search_client_patch.list_index_names.return_value = iter(['test'])
+        akc_patch.return_value = 'mock-key'
+        main(self.req)
+        sc_patch.assert_called()
+        sic_patch.assert_called()
+        akc_patch.assert_called()
+        akc_patch.assert_called()
+        cog_search_client_patch.list_index_names.assert_called()
+        prod_client_patch.upload_documents.assert_called()
+        verify_token_patch.assert_called_with('test')
+        http_response_patch.assert_called_with(
+                'Documents moved to production index',
+                status_code=200
+        )
+
+    def test_unauthorised(self):
+        """Tests that the endpoint returns a 401 when unauthorized"""
+        verify_token_patch.return_value = False
+        main(self.req)
+        verify_token_patch.assert_called_with('test')
+        http_response_patch.assert_called_with('Unauthorized', status_code=401)
+
+    @patch('core.validation_to_production.cognitiveSearchClient')
+    def test_validation_index_not_found(self, cog_search_client_patch):
+        """
+        Tests that the endpoint returns a 404 when validation index not found
+        """
+        verify_token_patch.return_value = True
+        cog_search_client_patch.list_index_names.return_value = iter(
+                ['not_test'])
+        main(self.req)
+        http_response_patch.assert_called_with(
+                'Validation index not found',
+                status_code=404
+        )
+
+    @patch('core.validation_to_production.cognitiveSearchClient')
+    def test_failed_delete_index(self, cog_search_client_patch):
+        """
+        Tests that the endpoint returns a 500 when deleting the validation
+        index fails
+        """
+        verify_token_patch.return_value = True
+        cog_search_client_patch.list_index_names.return_value = iter(['test'])
+        cog_search_client_patch.delete_index.side_effect = HttpResponseError(
+                'mock-error')
+        main(self.req)
+        http_response_patch.assert_called_with(
+                'Error deleting validation index: mock-error',
+                status_code=500
+        )
--- core/validation_to_production/__init__.py
+++ core/validation_to_production/__init__.py
@@ -1,4 +1,4 @@
-"""Azure Function to move vectors from validation to production index"""
+"""Azure Function to move vectors from validation to production inmex"""
 import os
 import logging
 
@@ -6,14 +6,17 @@ from azure.functions import HttpRequest, HttpResponse
 from azure.search.documents import SearchClient
 from azure.search.documents.indexes import SearchIndexClient
 from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+
+from utils.verify_token import verify_token
 
 # Get the service endpoint and API key from the environment
 
-CognitiveSearchServiceEndpoint = os.environ["CognitiveSearchServiceEndpoint"]
+CognitiveSearchServiceEndpoint = os.environ["CognitiveSearchEndpoint"]
 CognitiveSearchApiKey = os.environ["CognitiveSearchKey"]
 ProductionIndexName = os.environ["ProductionIndexName"]
 
-# Create a client
+# Create required clients
 
 credential = AzureKeyCredential(CognitiveSearchApiKey)
 
@@ -33,25 +36,44 @@ def main(req: HttpRequest) -> HttpResponse:
     """Azure Function to move vectors from validation to production index"""
     req_body = req.get_json()
 
+    # Ensure that the sender is authorized to make this request
+    if not verify_token(req.headers['Auth-Token']):
+        return HttpResponse(
+            "Unauthorized",
+            status_code=401
+        )
+
     # Create validation index client
     validation_index_name = req_body.get('validationIndexName')
+
+    index_names = list(cognitiveSearchClient.list_index_names())
+    if validation_index_name not in index_names:
+        return HttpResponse(
+            "Validation index not found",
+            status_code=404
+        )
+
     validation_index_client = SearchClient(
         endpoint=CognitiveSearchServiceEndpoint,
         index_name=validation_index_name,
         credential=credential
     )
 
-    # Get the documents from the validation index
+    # Migrate documents from validation to production index
     documents: list[dict] = list(
         list(*validation_index_client.search(search_text="*").by_page())
     )
     logging.info(documents)
-
-    # Upload documents to production index
     productionClient.upload_documents(documents)
 
     # Delete validation index
-    cognitiveSearchClient.delete_index(validation_index_name)
+    try:
+        cognitiveSearchClient.delete_index(validation_index_name)
+    except HttpResponseError as err:
+        return HttpResponse(
+            f"Error deleting validation index: {err.message}",
+            status_code=500
+        )
 
     return HttpResponse(
         "Documents moved to production index",
