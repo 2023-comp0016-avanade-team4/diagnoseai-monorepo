4095af2200775d8167440d9d0f18cdf1aad119ea
James <lim.en.hui.james@gmail.com>
2024-02-12 14:22:49 +0000

    Address Prettier issues

--- src/components/new-message-form.test.tsx
+++ src/components/new-message-form.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
-import { NewMessageForm } from './new-message-form';
+import { render } from "@testing-library/react";
+import { NewMessageForm } from "./new-message-form";
 
-describe('NewMessageForm', () => {
-    it('Only accepts only images', ()=> {
-       const chat = render(<NewMessageForm/>);
-       const imageInput = chat.baseElement.getElementsByTagName('input').item(0);
-       expect(imageInput).toHaveAttribute('accept', '.png, .jpg, .jpeg');
-    })
+describe("NewMessageForm", () => {
+  it("Only accepts only images", () => {
+    const chat = render(<NewMessageForm />);
+    const imageInput = chat.baseElement.getElementsByTagName("input").item(0);
+    expect(imageInput).toHaveAttribute("accept", ".png, .jpg, .jpeg");
+  });
 });
