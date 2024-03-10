b8eb392ecd88d9e850241c02122ccbc62dfe366a
George Ghetiu <george@eduroam-int-dhcp-97-87-174.ucl.ac.uk>
2024-03-10 23:42:28 +0000

    improvements

--- src/pages/api/chatConnection.ts
+++ src/pages/api/chatConnection.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const {userId, getToken } = getAuth(req);
+  const { userId, getToken } = getAuth(req);
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
