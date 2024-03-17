b5973d002da49e87acc1c089429c86e834ed09b1
James <lim.en.hui.james@gmail.com>
2024-03-17 12:11:12 +0000

    Use task name

--- src/app/workorder/delete/page.tsx
+++ src/app/workorder/delete/page.tsx
@@ -43,7 +43,7 @@ const Page = () => {
       <form className="flex flex-col gap-4 w-100 h-100 jusity-items-center items-center place-content-center" onSubmit={handleSubmit}>
         <h1 className="text-4xl py-5">Delete a work order</h1>
         <Select items={workOrders} label="Work Order" placeholder="Select a work order" isRequired onChange={handleWorkOrderChange}>
-          {(workOrder) => <SelectItem key={workOrder.order_id} value={workOrder.order_id}>{workOrder.order_id.substring(0, 8)}</SelectItem>}
+          {(workOrder) => <SelectItem key={workOrder.order_id} value={workOrder.order_id}>{workOrder.task_name}</SelectItem>}
         </Select>
         <Button type="submit" color="danger">Delete</Button>
       </form>
