4d06710a150501ac2cb1934bfee0ead800178ab6
James <lim.en.hui.james@gmail.com>
2024-03-17 09:29:28 +0000

    Upload success tweaks

--- src/app/uploadsuccess/page.tsx
+++ src/app/uploadsuccess/page.tsx
@@ -39,13 +39,18 @@ export default function UploadSuccess() {
   return (
     <div className="flex flex-col items-center justify-center h-full">
       <div className="mb-2">
-        <Image src={checkmark} alt="checkmark" className="max-w-xs" />
+        <Image src={checkmark} alt="checkmark" className="max-w-xs" width={200} height={200} />
       </div>
-      <p className="text-large text-center">
-        Your submission will be processed. We will send you an email once you are done; you can safely close this tab. Alternatively, please wait a moment, we will redirect you...(may take up to 5 minutes)
+      <p className="text-large text-center pb-5">
+        Your submission will be processed.
+      </p>
+      <p className="text-center">
+        We will send you an email once we&apos;re done.<br />
+        You can safely close this tab.<br />
+        Alternatively, please wait a moment, we will redirect you... (may take up to 5 minutes)
       </p>
 
       <Spinner />
-    </div>
+    </div >
   );
 }
