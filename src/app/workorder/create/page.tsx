a2d757d80df438bbd3b4134a224b8ad1e36cb39b
James <lim.en.hui.james@gmail.com>
2024-03-20 20:12:54 +0000

    Get more users (limit set to 1000)

--- src/app/workorder/create/page.tsx
+++ src/app/workorder/create/page.tsx
@@ -3,7 +3,7 @@ import ClientPage from './client-page';
 
 // This is a server-side component.
 const Page = async () => {
-  const users = await clerkClient.users.getUserList();
+  const users = await clerkClient.users.getUserList({ limit: 1000 });
   return <ClientPage users={users.map(user => ({
     id: user.id,
     email: user.emailAddresses[0].emailAddress,
